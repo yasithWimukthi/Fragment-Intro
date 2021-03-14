d5013c7e7b0ee18f7b78505201cf700bae16071f
yasithWimukthi <it19966922@my.sliit.lk>
2021-03-14 10:05:20 +0530

    Initial commit

---
+++ app/src/main/java/com/codeninja98/fragmentintro/MainFragment.java
@@ -0,0 +1,23 @@
+package com.codeninja98.fragmentintro;
+
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.Nullable;
+
+public class MainFragment extends Fragment {
+
+    public MainFragment() {
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_main,container,false);
+        return view;
+    }
+}
