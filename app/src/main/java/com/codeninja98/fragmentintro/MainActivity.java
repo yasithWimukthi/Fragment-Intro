386177148acd7d12d2eeb184dbed28614b6f93a4
yasithWimukthi <it19966922@my.sliit.lk>
2021-03-14 11:32:00 +0530

    create fragment programmatically

--- app/src/main/java/com/codeninja98/fragmentintro/MainActivity.java
+++ app/src/main/java/com/codeninja98/fragmentintro/MainActivity.java
@@ -1,6 +1,9 @@
 package com.codeninja98.fragmentintro;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+
 
 import android.os.Bundle;
 
@@ -10,5 +13,14 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //create fragment programmatically
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        Fragment fragment;
+
+        if(fragmentManager.findFragmentById(R.id.container)==null){
+            fragment = new MainFragment();
+            fragmentManager.beginTransaction().add(R.id.container,fragment).commit();
+        }
     }
 }
--- app/src/main/java/com/codeninja98/fragmentintro/MainFragment.java
+++ app/src/main/java/com/codeninja98/fragmentintro/MainFragment.java
@@ -1,17 +1,20 @@
 package com.codeninja98.fragmentintro;
 
 
-import android.app.Fragment;
+
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
 
 public class MainFragment extends Fragment {
 
     public MainFragment() {
+        super();
     }
 
     @Nullable
